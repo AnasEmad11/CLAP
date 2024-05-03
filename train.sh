91b5a19c605b9fefb2d74940e3a6f797d10f6785
Anas <anas.al-lahham@10.127.30.24>
2024-05-03 20:01:31 +0400

    final_v2

--- train.sh
+++ train.sh
@@ -1,27 +1,16 @@
-# python fedavg.py \
-#     -d ucf \
-#     -m c2fpl\
-#     --global_testset 0 \
-#     -bs 128 \
-#     --global_epoch 50 \
-#     --test_gap 50 \
-#     --local_epoch 5 \
-#     --partition scene_partition_chain_11_V3.pkl \
-
-python fedavg.py \
+python ./src/server/fedavg.py \
     -d ucf \
     -m c2fpl_ucf \
     --train_mode US \
     --global_testset 0 \
     -bs 128 \
-    --global_epoch 15\
-    --local_epoch 1\
+    --global_epoch 15 \
+    --local_epoch 1 \
     --partition ratio_partition_10_V3.pkl \
     --partition_chain ratio_partition_chain_10_V3.pkl \
     --video_num_partition ratio_video_num_partition_10_V3.pkl \
-    --test_gap 1\
-    --join_ratio 1\
-    --gmm_pl 1\
-    --eta_clustering 0\
-    --load 0\
-    --refine 0\
+    --test_gap 1 \
+    --join_ratio 1 \
+    --gmm_pl 0 \
+    --eta_clustering 0 \
+    --load 0 \
